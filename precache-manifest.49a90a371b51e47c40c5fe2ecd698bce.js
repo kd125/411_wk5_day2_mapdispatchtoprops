5553db290b1104547faa038a00b7c8e4d8e06eb2
Kd125 <kashdangar1250@gmail.com>
2023-06-14 20:55:22 -0500

    Updates

---
+++ precache-manifest.49a90a371b51e47c40c5fe2ecd698bce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3afa748ed5604d227f0e",
+    "url": "/static/css/main.006f9903.chunk.css"
+  },
+  {
+    "revision": "3afa748ed5604d227f0e",
+    "url": "/static/js/main.3afa748e.chunk.js"
+  },
+  {
+    "revision": "30bb281a593dfc16d7a1",
+    "url": "/static/js/1.30bb281a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "21207e56a577b849420d4a382ed42f48",
+    "url": "/index.html"
+  }
+];
